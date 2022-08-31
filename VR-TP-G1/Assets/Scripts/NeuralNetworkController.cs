19002e7beb476716c569d6271c177ec56233324f
eugepineiro <epineiro@itba.edu.ar>
2022-08-31 13:34:30 -0300

    Add Layer object

--- VR-TP-G1/Assets/Scripts/NeuralNetworkController.cs
+++ VR-TP-G1/Assets/Scripts/NeuralNetworkController.cs
@@ -10,20 +10,25 @@ public class NeuralNetworkController : MonoBehaviour
     {   
         network.Add(2);
         network.Add(3);
-        network.Add(3);
+        network.Add(5);
         network.Add(3);
         network.Add(2);
         int layers_amount = network.Count;
 
-        for(int layer = 0; layer < layers_amount; layer++){
+        for(int layer_index = 0; layer_index < layers_amount; layer_index++){
 
-            for(int neuron_index = 0; neuron_index < network[layer]; neuron_index++) {
+            GameObject layer = new GameObject(string.Format("Layer {0}", layer_index));
+            layer.transform.parent = transform; 
+            
+            for(int neuron_index = 0; neuron_index < network[layer_index]; neuron_index++) {
 
-                
                 GameObject neuron = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-                neuron.transform.parent = transform; 
-                neuron.transform.localPosition = new Vector3(layer, neuron_index,0); 
+                neuron.name = string.Format("Neuron {0}", neuron_index);
+                neuron.transform.parent = layer.transform; 
                 neuron.transform.localScale = new Vector3(0.2F, 0.2F, 0.2F); 
+
+                neuron.transform.localPosition = new Vector3(layer_index, neuron_index, 0); 
+                
             }
         }
 
