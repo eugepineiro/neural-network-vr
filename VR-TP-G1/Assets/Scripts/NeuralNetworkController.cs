d4e5e9d6ee0a18627f43fa282394e8be49fab703
eugepineiro <epineiro@itba.edu.ar>
2022-09-03 22:18:51 -0300

    Add labels

--- VR-TP-G1/Assets/Scripts/NeuralNetworkController.cs
+++ VR-TP-G1/Assets/Scripts/NeuralNetworkController.cs
@@ -14,7 +14,7 @@ public class NeuralNetworkController : MonoBehaviour{
 
     private enum NetworkType { MLP, AUTOENCODER, KOHONEN };
 
-    private NetworkType network_type = NetworkType.KOHONEN;
+    private NetworkType network_type = NetworkType.MLP;
 
     private int[,] kohonen_activations = { { 1, 2,4 }, { 3, 5,5 }, { 1, 6, 6 } }; // TODO esto deberia ser userInput 
     private int kohonen_input_dimension = 10;  // TODO esto deberia ser userInput 
@@ -84,6 +84,9 @@ public class NeuralNetworkController : MonoBehaviour{
             neuron.transform.localScale = new Vector3(0.2F, 0.2F, 0.2F);
             neuron.GetComponent<MeshRenderer>().material = neuronMaterial;
             neuron.transform.localPosition = new Vector3(layer_index, neuron_index - (network[layer_index]-1)/2.0f, 0);
+
+            generateLabels(neuron,  string.Format("({0};{1})", layer_index, neuron_index));
+
         }
         return layer;
     }
@@ -118,18 +121,30 @@ public class NeuralNetworkController : MonoBehaviour{
         JsonData json = JsonUtility.FromJson<JsonData>(jsonFile.text);
         return json;
     }
+
+    private void generateLabels(GameObject neuron, string text){ 
+         
+            GameObject label = new GameObject();
+             
+            label.transform.parent = neuron.transform;
+            label.name = "Label";
+
+            //Create TextMesh and modify its properties
+            TextMesh textMesh = label.AddComponent<TextMesh>();
+            textMesh.text = text;
+            textMesh.characterSize = 0.1F;
+
+            //Set postion of the TextMesh same as Neuron
+            textMesh.anchor = TextAnchor.MiddleCenter;
+            textMesh.alignment = TextAlignment.Center;
+            textMesh.transform.position = new Vector3(neuron.transform.position.x, neuron.transform.position.y, neuron.transform.position.z);
+    }
     
     private void BuildKohonen(int neurons_amount, int[,] activations) { 
         
       
         int height = activations.GetLength(0);
         int width = activations.GetLength(1);
-
-        /*for (int i=0; i < height; i++) {  
-            for (int j=0; j < width; j++) { 
-                neurons_amount += activations[i,j];
-            }
-        }*/
 
         // First Layer
         GameObject layer = createLayer(0);
@@ -150,7 +165,9 @@ public class NeuralNetworkController : MonoBehaviour{
         top_neurons.transform.parent = last_layer.transform;
         for (int i = 0; i < width; i++)
             generateKohonenTopLayerColumn(top_neurons, height, i, width);
+        
         addConnections(0, 1);
+
     }
 
     private void generateKohonenTopLayerColumn(GameObject layer, int rows_amount, int column, int width)
@@ -162,6 +179,8 @@ public class NeuralNetworkController : MonoBehaviour{
             neuron.transform.localScale = new Vector3(0.2F, 0.2F, 0.2F); 
             neuron.GetComponent<MeshRenderer>().material = neuronMaterial;
             neuron.transform.localPosition = new Vector3(5, neuron_index - (rows_amount - 1)/2.0f, column - (width - 1) / 2.0f);
+
+            generateLabels(neuron, string.Format("(1;{0};{1})", column, neuron_index));
         }
     }
 }
